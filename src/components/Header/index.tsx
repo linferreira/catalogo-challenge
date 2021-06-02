1cbb5ddf8d0f32b30f1d2f30c02b6cd2f2f0a914
Lindsay Ferreira <lindsay.ferreira@tegra.com.br>
2021-06-01 21:30:30 -0300

    feat: criar componente Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,11 +3,14 @@ import { Header } from './styles';
 
 interface IProps {
     label?: string;
+    size?: string;
 }
 
 const HeaderComponent = (props: IProps) => {
-    return <Header>
-        {props.label}
+    return <Header size={props.size}>
+        <h1>
+            {props.label}
+        </h1>
     </Header>
 }
 
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,17 +1,25 @@
 
 import styled from "styled-components";
 
-export const Header = styled.header`
-    width: 100%;
-    height: 70px;
+interface IPropsHeader {
+    size?: string
+}
 
+export const Header = styled.header<IPropsHeader>`
+    width: 100%;
+    height: ${(props) => props.size === 'small' ? '60px' : "70px"};
+    
     display: flex;
-    align-items: center;
-    justify-content: center;
+    align-items: ${(props) => props.size === 'small' ? 'flex-end' : "center"};
+    justify-content: ${(props) => props.size === 'small' ? 'start' : "center"};
 
     background-color: #116193;
 
-    color: #00E8E4;
-    font-size: 2em;
-    font-family: 'Abel';
+    h1 { 
+        margin-left: ${(props) => props.size === 'small' ? "3.5em" : 0};
+        color: #00E8E4;
+        font-weight: 300;
+        font-size: 2em;
+        font-family: 'Abel';
+    }
 `
